db7940a5456c9adf2e8ecde0525ac64886841efb
yangchanghun <gory4848@naver.com>
2026-01-23 15:26:50 +0900

    배포

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: "/", // 🔥 이거 반드시 필요
   plugins: [react()],
   resolve: {
     alias: {
