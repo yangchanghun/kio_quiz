ca450632c419ca11ba569fb6f6bbfa8ccb6663a5
yangchanghun <gory4848@naver.com>
2026-01-23 16:40:48 +0900

    D

--- be/config/urls.py
+++ be/config/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # 관리자
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
 
     # 앱별 URL
     path("api/accounts/", include("accounts.urls")),
