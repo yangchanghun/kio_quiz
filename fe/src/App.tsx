951fec81a1955dac04db0876ae78cb37bfa52ca0
yangchanghun <gory4848@naver.com>
2026-01-26 13:44:20 +0900

    D

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,79 +1,90 @@
-import { Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
-import { useAuthStore } from "./store/authStore";
-import MainPage from "./pages/MainPage";
-import AdminPage from "./pages/AdminPage";
-import { MainQuizListPage } from "./pages/Main/MainQuizListPage";
-import { ProtectedRoute } from "./routes/ProtectedRoute";
-import { Background } from "./background/Background";
-import { MainQuizDetailPage } from "./pages/Main/MainQuizDetailPage";
-import AdminQuizUpdator from "./pages/AdminQuizUpdator";
+// import { Route, Routes } from "react-router-dom";
+// import { useEffect } from "react";
+// import { useAuthStore } from "./store/authStore";
+// import MainPage from "./pages/MainPage";
+// import AdminPage from "./pages/AdminPage";
+// import { MainQuizListPage } from "./pages/Main/MainQuizListPage";
+// import { ProtectedRoute } from "./routes/ProtectedRoute";
+// import { Background } from "./background/Background";
+// import { MainQuizDetailPage } from "./pages/Main/MainQuizDetailPage";
+// import AdminQuizUpdator from "./pages/AdminQuizUpdator";
 
 function App() {
-  const initialize = useAuthStore((state) => state.initialize);
+  // const initialize = useAuthStore((state) => state.initialize);
 
-  useEffect(() => {
-    initialize();
-  }, []);
+  // useEffect(() => {
+  //   initialize();
+  // }, []);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Background>
-            <MainPage />
-          </Background>
-        }
-      />
+    // <Routes>
+    //   <Route
+    //     path="/"
+    //     element={
+    //       <Background>
+    //         <MainPage />
+    //       </Background>
+    //     }
+    //   />
 
-      <Route
-        path="/quizlist"
-        element={
-          <ProtectedRoute>
-            <Background>
-              <MainQuizListPage />
-            </Background>
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/quiz/:quizId"
-        element={
-          <ProtectedRoute>
-            <Background>
-              <MainQuizDetailPage />
-            </Background>
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/quiz/result"
-        element={
-          <ProtectedRoute>
-            <Background>
-              <MainQuizDetailPage />
-            </Background>
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/user/management"
-        element={
-          <ProtectedRoute>
-            <AdminPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/user/management/update/:quizId"
-        element={
-          <ProtectedRoute>
-            <AdminQuizUpdator />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+    //   <Route
+    //     path="/quizlist"
+    //     element={
+    //       <ProtectedRoute>
+    //         <Background>
+    //           <MainQuizListPage />
+    //         </Background>
+    //       </ProtectedRoute>
+    //     }
+    //   />
+    //   <Route
+    //     path="/quiz/:quizId"
+    //     element={
+    //       <ProtectedRoute>
+    //         <Background>
+    //           <MainQuizDetailPage />
+    //         </Background>
+    //       </ProtectedRoute>
+    //     }
+    //   />
+    //   <Route
+    //     path="/quiz/result"
+    //     element={
+    //       <ProtectedRoute>
+    //         <Background>
+    //           <MainQuizDetailPage />
+    //         </Background>
+    //       </ProtectedRoute>
+    //     }
+    //   />
+    //   <Route
+    //     path="/user/management"
+    //     element={
+    //       <ProtectedRoute>
+    //         <AdminPage />
+    //       </ProtectedRoute>
+    //     }
+    //   />
+    //   <Route
+    //     path="/user/management/update/:quizId"
+    //     element={
+    //       <ProtectedRoute>
+    //         <AdminQuizUpdator />
+    //       </ProtectedRoute>
+    //     }
+    //   />
+    // </Routes>
+    <div
+      style={{
+        color: "white",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      Loading...
+    </div>
   );
 }
 
