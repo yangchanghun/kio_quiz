e12f987e0a8505d189224fd55605b34ec88a2b73
yangchanghun <gory4848@naver.com>
2026-01-26 15:30:19 +0900

    D

--- be/config/settings.py
+++ be/config/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ),
 }
 
